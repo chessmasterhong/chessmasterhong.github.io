3c47c1c0395fa1f85ba724c55362a5783a70825d
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-05 15:10:07 -0500

    Completed portfolio item generation script.

--- tasks/generate-portfolio-items.js
+++ tasks/generate-portfolio-items.js
@@ -4,55 +4,78 @@ var fs = require('fs'),
     path = require('path');
 
 var srcRoot = '../src/';
+var destRoot = '../dist/';
 
 var charset = 'utf8';
-
 
 var matchTag = function(tag) {
     tag = tag.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
     return new RegExp('\\{\\{\\s*' + tag + '\\s*\\}\\}', 'g');
 };
 
-fs.readFile(srcRoot + 'templates/portfolio-item.html', charset,
-    function(err, html) {
-        if(!err) {
-            var i;
+var portfolioItems = '';
 
-            var projectData = JSON.parse(
-                fs.readFileSync(srcRoot + 'data/projects.json')
-            );
+var portfolioTemplate = fs.readFileSync(
+    path.join(__dirname, srcRoot, 'index.html'),
+    charset
+);
 
-            var projectItem = html;
+var portfolioItemTemplate = fs.readFileSync(
+    path.join(__dirname, srcRoot, 'templates/portfolio-item.html'),
+    charset
+);
 
-            for(i = 0; i < projectData.length; i++) {
-                projectItem = html.replace(
-                    matchTag('title'),
-                    projectData[i].title || ''
-                )
-                .replace(
-                    matchTag('caption'),
-                    projectData[i].caption || ''
-                )
-                //.replace(
-                //    matchTag('readMoreUrl'),
-                //    projectData[i].readMoreUrl || '#'
-                //)
-                .replace(
-                    matchTag('demoUrl'),
-                    projectData[i].demoUrl || '#'
-                )
-                .replace(
-                    matchTag('srcUrl'),
-                    projectData[i].srcUrl || '#'
-                )
-                .replace(
-                    matchTag('thumbnailUrl'),
-                    projectData[i].thumbnailUrl || '//placehold.it/400x400'
-                );
+var projectData = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, srcRoot, 'data/projects.json'),
+        charset
+    )
+);
 
-                console.log(projectItem);
-            }
-        } else {
+var i;
+
+for(i = 0; i < projectData.length; i++) {
+    var projectItem = '';
+    
+    projectItem = portfolioItemTemplate.replace(
+        matchTag('title'),
+        projectData[i].title || ''
+    )
+    .replace(
+        matchTag('caption'),
+        projectData[i].caption || ''
+    )
+    //.replace(
+    //    matchTag('readMoreUrl'),
+    //    projectData[i].readMoreUrl || '#'
+    //)
+    .replace(
+        matchTag('demoUrl'),
+        projectData[i].demoUrl || '#'
+    )
+    .replace(
+        matchTag('srcUrl'),
+        projectData[i].srcUrl || '#'
+    )
+    .replace(
+        matchTag('thumbnailUrl'),
+        projectData[i].thumbnailUrl || '//placehold.it/400x400'
+    );
+
+    portfolioItems += projectItem;
+}
+
+portfolioTemplate = portfolioTemplate.replace(
+    /((.|\n)*<ul.*id="portfolio-items".*?>).*(?=<\/ul>)/,
+    '$1' + portfolioItems
+);
+
+fs.writeFileSync(
+    path.join(__dirname, destRoot, 'index.html'),
+    portfolioTemplate,
+    charset,
+    function(err) {
+        if(err) {
             console.log(err);
         }
     }
