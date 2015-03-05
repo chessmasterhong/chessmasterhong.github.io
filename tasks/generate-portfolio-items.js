3b9fe96a2b2f17e2d29722a2ab4181c175842d04
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-05 13:13:40 -0500

    Implemented basic tag matching functionality to portfolio item generation script.

--- tasks/generate-portfolio-items.js
+++ tasks/generate-portfolio-items.js
@@ -7,13 +7,31 @@ var srcRoot = '../src/';
 
 var charset = 'utf8';
 
+
+var matchTag = function(identifier) {
+    return new RegExp(
+        '\\{\\{\\s*' +
+        identifier.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') +
+        '\\s*\\}\\}',
+        'g'
+    );
+};
+
 fs.readFile(srcRoot + 'templates/portfolio-item.html', charset,
     function(err, html) {
         if(!err) {
             var projectData = JSON.parse(
                 fs.readFileSync(srcRoot + 'data/projects.json')
             );
-            console.log(projectData);
+
+            var projectItem = html.replace(
+                matchTag('title'),
+                projectData[0].title
+            );
+
+
+
+            console.log(projectItem);
         } else {
             console.log(err);
         }
