dd7551f31c8f1e5959f72471436d01b577d435a4
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-05 11:39:15 -0500

    Reverted project to pre-RequireJS and pre-Backbone. Removed RequireJS and Backbone.

---
+++ src/scripts/captions.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    'use strict';
+    
+    $('.portfolio-item').hover(
+        function() {
+            $(this).find('.item-caption').fadeIn(250);
+        },
+        function() {
+            $(this).find('.item-caption').fadeOut(250);
+        }
+    );
+});
