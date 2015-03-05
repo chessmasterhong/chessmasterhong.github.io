413b4615f87d34897c65d858ac721c43f8693de4
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-05 11:39:43 -0500

    Added Less compilation gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
+    less = require('gulp-less'),
     webserver = require('gulp-webserver');
 
 gulp.task('lint', function() {
     return gulp.src('./src/scripts/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter(require('jshint-stylish')));
+});
+
+gulp.task('less', function() {
+    return gulp.src('./src/styles/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist/styles'));
 });
 
 gulp.task('default', function() {
