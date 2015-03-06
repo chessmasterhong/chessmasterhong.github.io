81282616863e0e194948e6e6dadb94830717010b
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-06 09:47:03 -0500

    Added del, gulp-preprrceoss, sulp-shell, and run-sequence to Node Dependencies. Added build processes for html and styles. Added copy and clean gulp tasks. Renamed tasks/generate-portfolio-items.js to tasks/generate-portfolio.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
+    del = require('del'),
     jshint = require('gulp-jshint'),
     less = require('gulp-less'),
+    preprocess = require('gulp-preprocess'),
+    runSequence = require('run-sequence'),
+    shell = require('gulp-shell'),
     webserver = require('gulp-webserver');
 
 gulp.task('lint', function() {
@@ -11,17 +15,45 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter(require('jshint-stylish')));
 });
 
-gulp.task('less', function() {
+gulp.task('build:styles', function() {
     return gulp.src('./src/styles/**/*.less')
         .pipe(less())
-        .pipe(gulp.dest('./dist/styles'));
+        .pipe(gulp.dest('./dist/styles/'));
+});
+
+gulp.task('build:html-generate', shell.task('node ./tasks/generate-portfolio.js'));
+
+gulp.task('build:html-process', function() {
+    return gulp.src('./dist/index.html')
+        .pipe(preprocess())
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build:copy', function() {
+    return gulp.src('./src/{scripts,styles,vendor}/**/*')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build:clean', function() {
+    return del.sync(['dist']);
+});
+
+gulp.task('build', function(cb) {
+    runSequence(
+        'build:clean',
+        'lint',
+        'build:copy',
+        ['build:html-generate', 'build:styles'],
+        'build:html-process',
+        cb
+    );
 });
 
 gulp.task('default', function() {
-    gulp.src('./src/')
+    gulp.src('./dist/')
         .pipe(webserver({
             host: '0.0.0.0',
-            port: 8080,
-            livereload: true
+            port: 8080
+            //livereload: true
         }));
 });
