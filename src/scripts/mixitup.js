6d977a8dc8df837e66be71ec5005d0d786c3ecb6
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-11 13:28:11 -0400

    Added additional MixItUp configuration.

--- src/scripts/mixitup.js
+++ src/scripts/mixitup.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     'use strict';
 
     $('.portfolio-items').mixItUp({
+        animation: {
+            effects: 'fade',
+            easing: 'ease'
+        },
+        controls: {
+            active: 'active'
+        },
+        load: {
+            filter: 'all'
+        },
         selectors: {
             filter: '.portfolio-filter',
             sort: '',
