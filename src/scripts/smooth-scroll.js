8c9fc26f3b9cd454da800f4d28f81c6300fc1de5
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-15 20:42:37 -0400

    Added basic smooth scroll to anchor script.

---
+++ src/scripts/smooth-scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    'use strict';
+
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+});
