58d3b7fe7b49816fd6b33f7825fdd207a86b854c
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-25 17:19:13 -0400

    Added stagger timer to portfolio item fade in effect.

--- src/scripts/scroll-fade-in.js
+++ src/scripts/scroll-fade-in.js
@@ -6,11 +6,15 @@
 
     var fadeIn = function() {
         panels.each(function(index) {
+            var t = 50;
+            var dt = parseInt(index * t);
+
             var a = $(this).offset().top + $(this).height() / 2;
             var b = $(window).scrollTop() + $(window).height();
 
             if(a < b) {
                 $(this)
+                    .delay(t + dt)
                     .animate({ opacity: 1 }, 'slow');
             }
         });
