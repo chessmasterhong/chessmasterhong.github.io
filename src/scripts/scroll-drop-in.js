30976bb18b438cd26a05676fc8455bf4f0c03a78
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-25 17:17:28 -0400

    Added drop in (slide and fade in) effect to showcase images.

---
+++ src/scripts/scroll-drop-in.js
@@ -0,0 +1,42 @@
+(function() {
+    'use strict';
+
+    var imgsLeft = $('#showcase .row-odd .showcase-thumbnail img')
+                    .css('opacity', 0)
+                    .css('left', '-40px');
+
+    var imgsRight = $('#showcase .row-even .showcase-thumbnail img')
+                    .css('opacity', 0)
+                    .css('right', '-40px');
+
+    var dropIn = function() {
+        imgsLeft.each(function() {
+            var a = $(this).offset().top + $(this).height() / 2;
+            var b = $(window).scrollTop() + $(window).height();
+
+            if(a < b) {
+                $(this)
+                    .animate({ opacity: 1, left: 0 }, 'slow');
+            }
+        });
+
+        imgsRight.each(function() {
+            var a = $(this).offset().top + $(this).height();
+            var b = $(window).scrollTop() + $(window).height();
+
+            if(a < b) {
+                $(this)
+                    .animate({ opacity: 1, right: 0 }, 'slow');
+            }
+        });
+    };
+
+
+    $(window).ready(function() {
+        dropIn();
+    });
+
+    $(window).scroll(function() {
+        dropIn();
+    });
+})();
