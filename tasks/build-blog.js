6a19b49449b473e1aa8672fd4e143cb36d8cb157
Kevin Chan <chessmasterhong@yahoo.com>
2015-04-14 00:31:32 -0400

    Added markdown to HTML conversion to raw blog files.

--- tasks/build-blog.js
+++ tasks/build-blog.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs'),
+    marked = require('marked'),
     path = require('path');
 
 var postsDir = path.join(__dirname, '..', 'src', 'data', 'blog', 'posts');
@@ -16,7 +17,8 @@ fs.readdir(
                 path.join(postsDir, file),
                 charset,
                 function(err, contents) {
-                    console.log(contents);
+                    var markdown = marked(contents);
+                    console.log(markdown);
                 }
             );
         });
